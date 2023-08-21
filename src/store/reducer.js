0627f2ed23393e010339c83e642bb62906a5941e
Noor Eldin El Sherbiny <noureldinelsherbiny5@gmail.com>
2023-08-22 02:05:35 +0300

    function for calculating rover step, adding Obstacles

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,6 +4,7 @@ const initialState = {
 	x_value: 0,
 	y_value: 0,
 	direction: "NORTH",
+    obstacles: [],
 };
 const directions = ["NORTH", "EAST", "SOUTH", "WEST"];
 
@@ -14,46 +15,49 @@ const roverDirections = {
 	r: directions[1],
 	d: "y",
 };
+function calculateNextStep(state,action){
+    let change_direction_flag = String(roverDirections[action.payload]).match("^[0-9 -]+$")? true: false;
+    if (change_direction_flag) {
+        let stepValue = roverDirections[action.payload];
+        // Negative Axis
+        if (state.direction === "SOUTH" || state.direction === "WEST") {
+            stepValue *= -1;
+        }
+        if (roverDirections.d === "x") {
+            let x_val = stepValue + (state.x_value?state.x_value:0);
+            return {
+                ...state,
+                x_value: x_val,
+            };
+        } else {
+            let y_val = stepValue + (state.y_value?state.y_value:0);
 
+            return {
+                ...state,
+                y_value: y_val,
+            };
+        }
+    } else {
+        let dir = roverDirections[action.payload];
+        let dir_index = directions.indexOf(dir);
+        roverDirections.r = directions[(dir_index + 1) % 4];
+        roverDirections.l = directions[(dir_index + 3) % 4];
+        roverDirections.d = dir_index % 2 != 0 ? "x" : "y";
+
+        return {
+            ...state,
+            direction: dir,
+        };
+    }
+}
 export default function reducer(state = initialState, action) {
 	switch (action.type) {
 		case actionType.submitData: {
-			let change_direction_flag = String(roverDirections[action.payload]).match("^[0-9 -]+$")? true: false;
-			if (change_direction_flag) {
-				let stepValue = roverDirections[action.payload];
-				if (state.direction === "SOUTH" || state.direction === "WEST") {
-					stepValue *= -1;
-				}
-				if (roverDirections.d === "x") {
-					let x_val = stepValue + (state.x_value?state.x_value:0);
-					console.log("x_val", x_val);
-					return {
-						...state,
-						x_value: x_val,
-					};
-				} else {
-					let y_val = stepValue + (state.y_value?state.y_value:0);
-					console.log("y_val", y_val);
-
-					return {
-						...state,
-						y_value: y_val,
-					};
-				}
-			} else {
-				let dir = roverDirections[action.payload];
-				let dir_index = directions.indexOf(dir);
-				roverDirections.r = directions[(dir_index + 1) % 4];
-				roverDirections.l = directions[(dir_index + 3) % 4];
-				roverDirections.d = dir_index % 2 != 0 ? "x" : "y";
-				console.log("dir", dir);
-
-				return {
-					...state,
-					direction: dir,
-				};
-			}
+			return calculateNextStep(state,action);
 		}
+        case actionType.addObstacle: {
+            return {...state, obstacles: [...state.obstacles, action.payload]};
+        }
 		default: {
 			return {...state};
 		}
