cca115cefa548cb069a715a190c7f1cc496892c9
Noor Eldin El Sherbiny <noureldinelsherbiny5@gmail.com>
2023-08-22 00:57:52 +0300

    updating rover to view changes in state

--- src/components/Rover.js
+++ src/components/Rover.js
@@ -1,35 +1,46 @@
 import React from "react";
+import { connect } from "react-redux";
 
 class Rover extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			x_value: 0,
-			y_vlaue: 0,
+			y_value: 0,
 			direction: "NORTH",
 		};
 	}
 	
-    componentDidUpdate(prevProps) {
+	componentDidUpdate(prevProps) {
 		if (prevProps != this.props) {
 			this.setState({
 				x_value: this.props.x_value,
-				y_vlaue: this.props.y_value,
+				y_value: this.props.y_value,
 				direction: this.props.direction,
 			});
 		}
 	}
 
 	render() {
+		console.log(this.props)
+
 		return (
 			<div>
                 <p>
-                    the rover X position is {this.state.x_value + " "} 
-                    and Y position is {this.state.y_vlaue} 
-                    <br/> The Direction is  {this.state.direction}</p>
+                    the rover X position is {this.props.x_value + " "} 
+                    and Y position is {this.props.y_value} 
+                    <br/> The Direction is  {this.props.direction}</p>
 			</div>
 		);
 	}
 }
 
-export default Rover;
+const mapStateToProps = (state) => {
+	return {
+		x_value: state.x_value,
+		y_value: state.y_value,
+		direction: state.direction,
+	}
+}
+
+export default connect(mapStateToProps, null)(Rover);
