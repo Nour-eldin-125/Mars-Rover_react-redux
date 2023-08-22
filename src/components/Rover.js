d6af02d4fe35783ed9bd02fbf8b43f9f17903f86
Noor Eldin El Sherbiny <noureldinelsherbiny5@gmail.com>
2023-08-22 12:38:47 +0300

    modify rover to view obstacles

--- src/components/Rover.js
+++ src/components/Rover.js
@@ -4,32 +4,21 @@ import { connect } from "react-redux";
 class Rover extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			x_value: 0,
-			y_value: 0,
-			direction: "NORTH",
-		};
 	}
-	
 	componentDidUpdate(prevProps) {
 		if (prevProps != this.props) {
-			this.setState({
-				x_value: this.props.x_value,
-				y_value: this.props.y_value,
-				direction: this.props.direction,
-			});
+			console.log(this.props.x_value);
 		}
 	}
-
 	render() {
 		console.log(this.props)
-
 		return (
 			<div>
                 <p>
                     the rover X position is {this.props.x_value + " "} 
                     and Y position is {this.props.y_value} 
                     <br/> The Direction is  {this.props.direction}</p>
+					<p>Obstacles : {this.props.obstacles?.map((obs)=>"["+obs+"]")}</p>
 			</div>
 		);
 	}
@@ -40,6 +29,7 @@ const mapStateToProps = (state) => {
 		x_value: state.x_value,
 		y_value: state.y_value,
 		direction: state.direction,
+		obstacles: state.obstacles,
 	}
 }
 
