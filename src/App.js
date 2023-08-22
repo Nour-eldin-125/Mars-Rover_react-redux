ef2759569db90f58d5eb86c4166cb0ad1d39a18a
Noor Eldin El Sherbiny <noureldinelsherbiny5@gmail.com>
2023-08-22 20:00:49 +0300

    creating Visualization of the movment

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Rover from './components/Rover';
 import InputDirection from './components/InputDirection';
 import Obstacles from './components/Obstacles';
+import RoverGrid from './components/RoverGrid';
 
 function App() {
   return (
     <>
       <h1>Mars Rover</h1>
+      <RoverGrid />
       <InputDirection />
       <br/>
       <Obstacles />
---
+++ src/components/RoverGrid.js
@@ -0,0 +1,72 @@
+ import React from "react";
+ import { connect } from "react-redux";
+
+ class RoverGrid extends React.Component {
+ 	constructor(props) {
+        super(props);
+        
+        this.num = null
+        this.roverImgfromdirection={
+            "NORTH":"rover_top.png",
+            "EAST":"rover_right.png",
+            "SOUTH":"rover_bot.png",
+            "WEST":"rover_left.png"
+        }
+ 	}
+     componentDidMount() {
+         this.createRoverImg(this.props.x_value,this.props.y_value)
+        }
+    componentDidUpdate() {
+        this.createRoverImg(this.props.x_value,this.props.y_value)
+    }
+        
+    createRoverImg(x,y){
+        document.getElementById("img_"+this.num)?.setAttribute("src","")
+        let number = this.getNumberOfRovers(x,y);
+        let rover = document.getElementById("img_"+number);
+        rover.setAttribute("src", this.roverImgfromdirection[this.props.direction])
+        this.num = number
+    }
+    getNumberOfRovers(x,y){
+        while (x<0)
+            x+=10
+        while (y<0)
+            y+=10
+        x %= 10
+        y %= 10 
+        return y*10+x
+    }
+ 	render() {
+ 		return (
+ 			<>
+ 				<div className="grid-container">
+                    {
+                        [ ...Array(100).reverse().keys() ].map((i)=>{
+                            i=99-i
+                            let j =0
+                            i < 10 ? j=0 : j = parseInt(i / 10)
+                            j*=10
+                            i = i % 10
+                            let id = j+9-i
+                            return <div className="grid-item" id = {id} key={j+9-i}>
+                                <img id = {"img_"+id}/>
+                            </div>
+                        })
+                    }
+
+                </div>
+            </>
+        )
+    }
+}
+const mapStateToProps = (state) => {
+    return {
+        x_value: state.x_value,
+        y_value: state.y_value,
+        direction: state.direction,
+        obstacles: state.obstacles,
+        safe: state.safe,
+    }
+}
+
+export default connect(mapStateToProps, null)(RoverGrid);
