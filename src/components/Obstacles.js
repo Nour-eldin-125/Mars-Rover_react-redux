511ba59db8dcdc02796a7891cb8b1adadf9544d7
Noor Eldin El Sherbiny <noureldinelsherbiny5@gmail.com>
2023-08-22 12:39:26 +0300

    validate that obstacles not duplicated

--- src/components/Obstacles.js
+++ src/components/Obstacles.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { connect } from "react-redux";
+import { addObstacle } from "../store/actions";
+
 
 class Obstacles extends React.Component {
+    
+    addObs(){
+        let x_value = document.getElementById("inp_obstacle_x").value;
+        let y_value = document.getElementById("inp_obstacle_y").value;
+        let obs = [x_value,y_value];
+        if(this.props.obstacles.some(obs => obs[0] == obs[0] && obs[1] == obs[1])){
+            alert("obstacle already exists");
+            return
+        }
+        this.props.addObstacle(obs);
+    }
     
     render() {
         return (
@@ -10,10 +24,22 @@ class Obstacles extends React.Component {
                 <input type="text"  id="inp_obstacle_x"/>
                 <label> Y value : </label>
                 <input type="text"  id="inp_obstacle_y"/>
-                <button onClick= {() => {this.checkData()}}>Submit</button>
+                <button onClick= {() => {this.addObs()}}>Submit</button>
             </>
         )
     }
 }
 
-export default Obstacles;
+const mapStateToProps = (state) => {
+    return {
+        obstacles: state.obstacles,
+    }
+}
+
+const mapDispatchToProps = (dispatch)=>{
+    return {
+        addObstacle: (payload) => dispatch(addObstacle(payload))
+    }
+}
+
+export default connect (mapStateToProps, mapDispatchToProps)(Obstacles);
