19156378bc2ff2cb6f5cedae8fc65a492698f3e2
Noor Eldin El Sherbiny <noureldinelsherbiny5@gmail.com>
2023-08-27 02:16:26 +0300

    modifying UI

--- src/components/Obstacles.js
+++ src/components/Obstacles.js
@@ -5,26 +5,28 @@ import { addObstacle } from "../store/actions";
 
 class Obstacles extends React.Component {
     
-    addObs(){
-        let x_value = document.getElementById("inp_obstacle_x").value;
-        let y_value = document.getElementById("inp_obstacle_y").value;
-        let obs = [x_value,y_value];
+    
+    addObs() {
+        let inp = document.getElementById("inp_obstacle");
+        if (!(inp.value.match("^[0-9 -]+,[0-9 -]+$"))) {
+            alert("Invalid Input");
+            inp.value = ""
+            return
+        }
+        let obs = inp.value.split(",")
         if(this.props.obstacles.some(obstacle => obstacle[0] == obs[0] && obstacle[1] == obs[1])){
             alert("obstacle already exists");
             return
         }
         this.props.addObstacle(obs);
     }
-    
+
     render() {
         return (
             <>
-                <h3>Obstacles</h3>
-                <label>X value : </label>
-                <input type="number"  id="inp_obstacle_x"/>
-                <label> Y value : </label>
-                <input type="number"  id="inp_obstacle_y"/>
-                <button onClick= {() => {this.addObs()}}>Submit</button>
+                <label>Obstacles</label>
+                <input type="text"  id="inp_obstacle"/>                
+                <button className = "add obstacle" onClick= {() => {this.addObs()}}>ADD</button>
             </>
         )
     }
